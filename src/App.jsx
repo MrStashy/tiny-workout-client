cf8e79203de7bb01cdc5780821bd8205f9256240
Will Baxter <bax.will@googlemail.com>
2024-09-04 23:33:48 +0100

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,27 @@ import UserStats from "./pages/UserStats";
 import Dashboard from "./pages/Dashboard";
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
-import { UserProvider } from "./context/UserContext"
-import { ProtectedRoute } from "./context/ProtectedRoute"
-
+import { UserProvider } from "./context/UserContext";
+import { ProtectedRoute } from "./context/ProtectedRoute";
 
 function App() {
   const [justRegistered, setJustRegistered] = useState(false);
 
   return (
     <UserProvider>
-    <div className="h-screen bg-app-bg bg-cover">
-      <div className="h-screen flex flex-col absolute inset-0 bg-gradient-to-b from-tiny-orange/30 to-transparent  font-inter">
-        <Header />
-        <Routes>
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route
-            path="/register"
-            element={<Register setJustRegistered={setJustRegistered} />}
-          />
-          {justRegistered && (
-            <Route path="/user-details" element={<UserStats />} />
-          )}
-          <Route
+      <div className="h-screen bg-app-bg bg-cover">
+        <div className="h-screen flex flex-col absolute inset-0 bg-gradient-to-b from-tiny-orange/30 to-transparent  font-inter">
+          <Header />
+          <Routes>
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route
+              path="/register"
+              element={<Register setJustRegistered={setJustRegistered} />}
+            />
+            {justRegistered && (
+              <Route path="/user-details" element={<UserStats />} />
+            )}
+            <Route
               path="/dashboard"
               element={
                 <ProtectedRoute>
@@ -34,10 +33,10 @@ function App() {
                 </ProtectedRoute>
               }
             />
-          <Route path="*" element={<SignIn />} />
-        </Routes>
+            <Route path="*" element={<SignIn />} />
+          </Routes>
+        </div>
       </div>
-    </div>
     </UserProvider>
   );
 }
--- src/hooks/useUser.jsx
+++
@@ -1,8 +0,0 @@
-// import { useContext } from "react"
-// import { CurrentUserContext } from '../context/currentUser'
-
-// function useUser() {
-//     return useContext(CurrentUserContext)
-// }
-
-// export default useUser
