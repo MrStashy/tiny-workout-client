cbc0548313f8a648af639ded95dcdfc65582187c
Will Baxter <bax.will@googlemail.com>
2024-09-12 12:16:11 +0100

    workout details display

--- src/components/PaginationModule.jsx
+++ src/components/PaginationModule.jsx
@@ -1,11 +1,26 @@
-export default function PaginationModule({ pageNo, setPageNo }) {
+export default function PaginationModule({ pageNo, setPageNo, workoutsOnPage }) {
 
+
+    function handleUpClick() {
+        if (workoutsOnPage < 5) {
+            return
+        }
+        setPageNo(pageNo += 1)
+    }
+
+    function handleDownClick() {
+        if(pageNo === 1) {
+            return
+        }
+
+        setPageNo(pageNo -= 1)
+    }
 
     return(
         <section className="flex flex-row gap-2 place-items-center">
-        <img className="h-4" src="src/assets/SVG/left-arrow-pagination.svg" onClick={() => {setPageNo(pageNo -= 1)}}/>
+        <img className="h-4 cursor-pointer" src="src/assets/SVG/left-arrow-pagination.svg" onClick={handleDownClick}/>
         <p className="bg-white rounded-full w-8 text-center text-tiny-orange">{pageNo}</p>
-        <img className="h-4 cursor-pointer" src="src/assets/SVG/right-arrow-pagination.svg" onClick={() => {setPageNo(pageNo += 1)}}/>
+        <img className="h-4 cursor-pointer" src="src/assets/SVG/right-arrow-pagination.svg" onClick={handleUpClick}/>
         </section>
     )
 }
--- src/components/WorkoutHistoryCard.jsx
+++ src/components/WorkoutHistoryCard.jsx
@@ -3,10 +3,12 @@ import { UserContext } from "../context/UserContext.jsx";
 import { getWorkoutsByIdPaginated } from "../utils/apiFunctions.js";
 import PaginationModule from "./PaginationModule.jsx";
 import formatDate from "../utils/formatDate.js"
+import formatExerciseName from "../utils/formatExerciseName.js";
 
 export default function WorkoutHistoryCard() {
     const [pageOfWorkouts, setPageOfWorkouts] = useState([])
     const [pageNo, setPageNo] = useState(1)
+    const [clickedWorkoutId, setClickedWorkoutId] = useState(0)
 
     const userId = useContext(UserContext).currentUser.id
     const itemsPerPage = 5
@@ -21,20 +23,58 @@ export default function WorkoutHistoryCard() {
     }, [pageNo])
 
     function handleClick(e) {
-        console.log(e.target.id)
+        if (Number(e.target.id) === clickedWorkoutId) {
+            setClickedWorkoutId(0)
+            return
+        }
+        setClickedWorkoutId(Number(e.target.id))
     }
 
+    console.log(clickedWorkoutId)
     return (
-        <section className="bg-slate-200 w-full h-full max-h-[620px] rounded-lg flex flex-col place-items-center p-4 gap-4">
+        <section className="bg-slate-200 w-full h-full max-h-[620px] rounded-lg flex flex-col place-items-center justify-between p-4 gap-4 overflow-auto">
         <p className="rounded-md text-slate-400">Workout History</p>
         <ul className="flex flex-col gap-2">
         {pageOfWorkouts.length > 0 && pageOfWorkouts.map((workout) => {
             return(
-                <button className="border border-tiny-orange text-tiny-orange hover:text-white hover:bg-tiny-orange rounded-full w-64 h-12 font-light" key={workout.id} id={workout.id} onClick={handleClick}>{formatDate(workout.createdAt)}</button>
+                <li key={workout.id} className="flex flex-col gap-2">
+                <button className="border border-tiny-orange text-tiny-orange hover:text-white hover:bg-tiny-orange rounded-full w-64 h-12 font-light" id={workout.id} onClick={handleClick}>{formatDate(workout.createdAt)}</button>
+
+                {workout.id === clickedWorkoutId && 
+                <WorkoutDetails workout={workout} />
+                }
+                </li>
             )
         })}
+        {pageOfWorkouts.length === 0 && <p>No workouts!</p>}
         </ul>
-       <PaginationModule pageNo={pageNo} setPageNo={setPageNo}/>
+       <PaginationModule pageNo={pageNo} setPageNo={setPageNo} workoutsOnPage={pageOfWorkouts.length}/>
       </section>
+    )
+}
+
+function WorkoutDetails({workout}) {
+    const { exercises } = workout
+    
+    return (
+        <ul className="bg-white text-xs p-4 rounded-lg shadow-lg">
+            {exercises.map((exercise) => {
+                return (
+                    <li key={exercise.id} className="flex flex-col gap-1 mb-1">
+                        <p className="bg-tiny-orange text-white rounded-md p-1">{formatExerciseName(exercise.name)}</p>
+                        {exercise.sets.map((set, index) => {
+                            return (
+                                <ul key={set.id} className="flex flex-row gap-4 justify-between">
+                                <li >Set {index + 1}</li>
+                                <li>{set.weight /1000} kg</li>
+                                <li>Reps: {set.reps}</li>
+                                </ul>
+                            )
+                        })}
+
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
